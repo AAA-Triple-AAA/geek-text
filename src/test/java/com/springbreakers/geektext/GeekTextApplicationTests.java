5630b95109b6d07d207f8a961048d36d2f79ba2e
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-02-05 23:39:27 -0500

    initial commit

---
+++ src/test/java/com/springbreakers/geektext/GeekTextApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springbreakers.geektext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTextApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
