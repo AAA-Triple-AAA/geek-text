afbdb77ca1b68b6d85b775d4ca43519b5829f376
Adrian Abella <fasttaco1@gmail.com>
2025-02-07 13:08:12 -0500

    Database changes applied to migtration file

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,93 +1,99 @@
 -- Create publisher, author, and book first because of their close relationships
 CREATE TABLE publisher
 (
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    discount DECIMAL(5, 2)
 );
+
 CREATE TABLE author
 (
-    id           SERIAL PRIMARY KEY,
-    name_first   VARCHAR(255) NOT NULL,
-    name_last    VARCHAR(255) NOT NULL,
-    biography    TEXT,
-    publisher_id INTEGER      REFERENCES publisher (id) ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    biography TEXT,
+    publisher_id INTEGER REFERENCES publisher (id) ON DELETE SET NULL
 );
+
+CREATE TABLE genre
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
 CREATE TABLE book
 (
-    id           SERIAL PRIMARY KEY,
-    isbn         VARCHAR(13)  NOT NULL,
-    name         VARCHAR(255) NOT NULL,
-    description  TEXT,
-    year         INTEGER,
-    price        DECIMAL(10, 2),
-    discount     DECIMAL(5, 2),
-    copies_sold  INTEGER,
-    rating       DECIMAL(3, 2),
-    genre        VARCHAR(255),
-    publisher_id INTEGER      REFERENCES publisher (id) ON DELETE SET NULL,
-    author_id    INTEGER      REFERENCES author (id) ON DELETE SET NULL
+    isbn VARCHAR(13) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    year INTEGER,
+    price DECIMAL(10, 2),
+    copies_sold INTEGER,
+    genre_id INTEGER REFERENCES genre (id) ON DELETE SET NULL,
+    publisher_id INTEGER REFERENCES publisher (id) ON DELETE SET NULL,
+    author_id INTEGER REFERENCES author(id)
 );
 
 -- Create user and credit card because of their close relationship
 CREATE TABLE "user"
 (
-    id             SERIAL PRIMARY KEY,
-    username       VARCHAR(255) NOT NULL UNIQUE,
-    password       VARCHAR(255) NOT NULL,
-    name_first     VARCHAR(255) NOT NULL,
-    name_last      VARCHAR(255) NOT NULL,
-    email          VARCHAR(255) NOT NULL UNIQUE,
-    address_street VARCHAR(255),
-    address_city   VARCHAR(255),
-    address_state  VARCHAR(255),
-    address_zip    VARCHAR(20)
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    address TEXT
 );
+
 CREATE TABLE credit_card
 (
-    id          SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     card_holder VARCHAR(255) NOT NULL,
-    number      VARCHAR(16)  NOT NULL,
-    cvv         VARCHAR(3)   NOT NULL,
-    zip         VARCHAR(20)  NOT NULL,
-    user_id     INTEGER REFERENCES "user" (id) ON DELETE CASCADE
+    number VARCHAR(16) UNIQUE NOT NULL,
+    cvv VARCHAR(3)   NOT NULL,
+    zip VARCHAR(20)  NOT NULL,
+    user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE
 );
 
 -- Create shopping cart relation table
-CREATE TABLE has_in_cart
+CREATE TABLE shopping_cart
 (
     user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
-    book_id INTEGER REFERENCES book (id) ON DELETE CASCADE,
+    book_id VARCHAR(13) REFERENCES book (isbn) ON DELETE CASCADE,
     PRIMARY KEY (user_id, book_id)
 );
 
 -- Create rating and commenting together because they are closely related features
 CREATE TABLE rating
 (
-    id        SERIAL PRIMARY KEY,
-    rating    INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
-    datestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id   INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
-    book_id   INTEGER REFERENCES book (id) ON DELETE CASCADE
+    user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
+    book_id VARCHAR(13) REFERENCES book (isbn) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, book_id),
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
+    datestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE comment
 (
-    id        SERIAL PRIMARY KEY,
-    comment   TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    comment TEXT NOT NULL,
     datestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id   INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
-    book_id   INTEGER REFERENCES book (id) ON DELETE CASCADE
+    user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
+    book_id VARCHAR(13) REFERENCES book (isbn) ON DELETE CASCADE
 );
 
 -- Create wish_list related tables
-CREATE TABLE wish_list
+CREATE TABLE wishlist
 (
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL, -- may or may not have to be unique for entire user base --
     user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE
 );
-CREATE TABLE is_in_list
+
+CREATE TABLE wishlist_book
 (
-    wish_list_id INTEGER REFERENCES wish_list (id) ON DELETE CASCADE,
-    book_id      INTEGER REFERENCES book (id) ON DELETE CASCADE,
+    wish_list_id INTEGER REFERENCES wishlist (id) ON DELETE CASCADE,
+    book_id VARCHAR(13) REFERENCES book (isbn) ON DELETE CASCADE,
     PRIMARY KEY (wish_list_id, book_id)
 );
