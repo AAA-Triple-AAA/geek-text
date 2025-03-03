b8f9b3e3f2de7adf376cf9756161284c9cfee676
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-03-03 18:44:07 -0500

    Added book comments api endpoint
    Added the /books/{bookId}/comments api endpoint
    in the controller class and added the comment service.

--- src/main/java/com/springbreakers/geektext/controller/BookRatingAndCommentingController.java
+++ src/main/java/com/springbreakers/geektext/controller/BookRatingAndCommentingController.java
@@ -1,31 +1,35 @@
 package com.springbreakers.geektext.controller;
 
+import com.springbreakers.geektext.service.CommentService;
 import com.springbreakers.geektext.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springbreakers.geektext.model.Comment;
 import com.springbreakers.geektext.model.Rating;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ratings")
+@RequestMapping("/api")
 public class BookRatingAndCommentingController
 {
     private final RatingService ratingService;
+    private final CommentService commentService;
 
     @Autowired
-    public BookRatingAndCommentingController(RatingService ratingService) {
+    public BookRatingAndCommentingController(RatingService ratingService, CommentService commentService) {
         this.ratingService = ratingService;
+        this.commentService = commentService;
     }
 
-    @GetMapping
-    public List<Rating> getRatings() {
-        return ratingService.getRatings();
+    @GetMapping("/books/{bookId}/comments")
+    public List<Comment> getBookComments(@PathVariable int bookId) {
+        return commentService.getBookComments(bookId);
     }
-
     /*
     TODO:
     - Create rating for a book by a user on a 5-star scale with a datestamp
--- src/main/java/com/springbreakers/geektext/service/CommentService.java
+++ src/main/java/com/springbreakers/geektext/service/CommentService.java
@@ -3,17 +3,22 @@ package com.springbreakers.geektext.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.springbreakers.geektext.model.Rating;
+import com.springbreakers.geektext.model.Comment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class RatingService {
+public class CommentService {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public RatingService(JdbcTemplate jdbcTemplate) {
+    public CommentService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Comment> getBookComments(int bookId) {
+        String sql = "SELECT * FROM comment WHERE book_id=?";
+        return jdbcTemplate.query(sql, Comment.COMMENT_MAPPER, bookId);
     }
 }
