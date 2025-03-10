93723e2469a665a882dc7640eb026c56784a27df
bricmaya <mayaabriceno@gmail.com>
2025-03-10 00:38:27 -0400

    booksorting - sprint3

---
+++ src/main/java/com/springbreakers/geektext/service/BookSortingService.java
@@ -0,0 +1,27 @@
+package com.springbreakers.geektext.service;
+import com.springbreakers.geektext.model.BookSorting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class BookSortingService {
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public BookSortingService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public List<BookSorting> getAllBooks() {
+        String sql = "SELECT * FROM book";
+        return jdbcTemplate.query(sql, BookSorting.BOOK_MAPPER);
+    }
+    public List<BookSorting> getBooksByGenre(int genre_id) {
+        String sql = "SELECT * FROM book WHERE genre_id = ?";
+        return jdbcTemplate.query(sql, BookSorting.BOOK_MAPPER, genre_id);
+    }
+
+    public List<BookSorting> getTopSellers() {
+        String sql = "SELECT * FROM book ORDER BY copies_sold DESC LIMIT 10";
+        return jdbcTemplate.query(sql, BookSorting.BOOK_MAPPER);
+    }
+}
