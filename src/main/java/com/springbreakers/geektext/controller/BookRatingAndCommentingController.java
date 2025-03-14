e1789115a9d4cbd7895e22bf659259d70361f106
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-03-13 23:59:53 -0400

    Add feature for adding a book rating

--- src/main/java/com/springbreakers/geektext/controller/BookRatingAndCommentingController.java
+++ src/main/java/com/springbreakers/geektext/controller/BookRatingAndCommentingController.java
@@ -2,18 +2,24 @@ package com.springbreakers.geektext.controller;
 
 import com.springbreakers.geektext.service.CommentService;
 import com.springbreakers.geektext.service.RatingService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.springbreakers.geektext.model.Comment;
 import com.springbreakers.geektext.model.Rating;
 
+import java.net.URI;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -60,9 +66,57 @@ public class BookRatingAndCommentingController
         return ResponseEntity.ok(comments);
     }
 
+    @PostMapping("/books/{bookId}/rating")
+    public ResponseEntity<?> addBookRating(@PathVariable String bookId, @RequestParam String userId, @RequestParam String rating) {
+        int validBookId, validUserId, validRating;
+
+        try {
+            validBookId = Integer.parseInt(bookId);
+            validUserId = Integer.parseInt(userId);
+            validRating = Integer.parseInt(rating);
+        } catch (NumberFormatException e) {
+            throw new DataIntegrityViolationException("Invalid format");
+        }
+
+        Optional<Rating> newRating = ratingService.addBookRating(validBookId, validUserId, validRating);
+        if (newRating.isEmpty()) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.created(URI.create("/books/" + bookId + "/rating")).build();
+    }
+
+    @ResponseStatus(value=HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    @ResponseBody
+    public Map<String, String> dataConflict() {
+       return Map.of("error", "Invalid format or data does not exist");
+    }
+
+    @ResponseStatus(value=HttpStatus.CONFLICT)
+    @ExceptionHandler({SQLException.class, DuplicateKeyException.class})
+    @ResponseBody
+    public Map<String, String> alreadyExists() {
+        return Map.of("error", "Resource already exists");
+    }
+
+    @ResponseStatus(value=HttpStatus.NOT_FOUND)
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    @ResponseBody
+    public Map<String, String> resourceNotFound() {
+        return Map.of("error", "Resource not found");
+    }
+
+    @ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({Exception.class})
+    @ResponseBody
+    public Map<String, String> otherErrors() {
+        return Map.of("error", "Internal server error");
+    }
+
+
     /*
     TODO:
-    - Create rating for a book by a user on a 5-star scale with a datestamp
     - Create comment for a book by a user with a datestamp
      */
 }
--- src/main/java/com/springbreakers/geektext/service/RatingService.java
+++ src/main/java/com/springbreakers/geektext/service/RatingService.java
@@ -1,12 +1,15 @@
 package com.springbreakers.geektext.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.springbreakers.geektext.model.Rating;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RatingService {
@@ -20,5 +23,24 @@ public class RatingService {
     public Double getBookAvgRating(int bookId) {
         String sql = "SELECT AVG(rating) FROM rating WHERE book_id=?";
         return jdbcTemplate.queryForObject(sql, Double.class, bookId);
+    }
+
+    public Optional<Rating> addBookRating(int bookId, int userId, int rating) {
+        String sql = "INSERT INTO rating (rating, user_id, book_id) VALUES (?, ?, ?)";
+        int rowsChanged = jdbcTemplate.update(sql, rating, userId, bookId);
+        if (rowsChanged == 1) {
+            return getBookRating(bookId, userId);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Rating> getBookRating(int bookId, int userId) {
+        String sql = "SELECT * FROM rating WHERE book_id=? AND user_id=?";
+        try {
+            Rating rating = jdbcTemplate.queryForObject(sql, Rating.RATING_MAPPER, bookId, userId);
+            return Optional.of(rating);
+        } catch (Exception e) {
+           return Optional.empty();
+        }
     }
 }
