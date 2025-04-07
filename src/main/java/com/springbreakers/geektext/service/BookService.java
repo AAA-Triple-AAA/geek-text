7764a0fc5c361d19e1bf1cc41f7b4194a593366d
JA5988 <jalonso5988@gmail.com>
2025-04-06 20:03:16 -0400

    Sprint 5 - Swagger Documentation / Javadoc

--- src/main/java/com/springbreakers/geektext/service/BookService.java
+++ src/main/java/com/springbreakers/geektext/service/BookService.java
@@ -32,6 +32,12 @@ public class BookService {
     }
 
     //JESSICA'S METHODS
+    /**
+     * Service method that retrieves a book by its ISBN.
+     *
+     * @param isbn The ISBN of the book.
+     * @return ResponseEntity containing the book or NOT_FOUND status if not found.
+     */
     public ResponseEntity<?> getBookByISBN(String isbn) {
         String sql = "SELECT * FROM book WHERE isbn = ?";
         List<Book> books = jdbcTemplate.query(sql, Book.BOOK_MAPPER, isbn);
@@ -39,6 +45,12 @@ public class BookService {
                 : ResponseEntity.ok(books.get(0));
     }
 
+    /**
+     * Service method that retrieves all books by a specific author.
+     *
+     * @param authorId The ID of the author.
+     * @return ResponseEntity with a list of books or NOT_FOUND status if no books exist.
+     */
     public ResponseEntity<?> getBooksByAuthor(int authorId) {
         String sql = "SELECT * FROM book WHERE author_id = ?";
         List<Book> books = jdbcTemplate.query(sql, Book.BOOK_MAPPER, authorId);
@@ -46,6 +58,12 @@ public class BookService {
                 : ResponseEntity.ok(books);
     }
 
+    /**
+     * Service method that creates a new book entry in the database.
+     *
+     * @param book Book object containing all required data to be inserted.
+     * @return ResponseEntity with success, conflict, or error message depending on outcome.
+     */
     public ResponseEntity<?> createBook(Book book) {
         String checkSql = "SELECT COUNT(*) FROM book WHERE isbn = ?";
         int count = jdbcTemplate.queryForObject(checkSql, Integer.class, book.getIsbn());
@@ -77,6 +95,11 @@ public class BookService {
     }
 
     // GENERAL METHODS
+    /**
+     * Service method that retrieves all books from the database.
+     *
+     * @return List of all books.
+     */
     public List<Book> getAllBooks() {
         String sql = "SELECT * FROM book";
         return jdbcTemplate.query(sql, Book.BOOK_MAPPER);
